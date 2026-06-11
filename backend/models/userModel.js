f1d69151d7a7ef61f4516ef056a1b76f5ee47875
Stephen Anti <stephenanti63@gmail.com>
2026-06-11 01:03:03 -0700

    1-added message fields

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,32 +2,34 @@ import { Schema } from "mongoose";
 
 const userModel = new Schema({
     fullname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    faculty: {
-        type: String,
-        required: true
-    },
-    level: {
-        type: Number,
-        required: true
-    },
-    gender: {
       type: String,
       required: true
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    faculty: {
+      type: String,
+      required: true
+    },
+    level: {
+      type: Number,
+      required: true
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"]
+    },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     profilePic: {
       type: String,
+      default: ""
     },
     role: {
       type: String,
