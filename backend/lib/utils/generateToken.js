1f640b0276a38f71fa893ae710e1c5c7c168ec56
Stephen Anti <stephenanti63@gmail.com>
2026-06-16 16:43:46 -0700

    1-jwt token logic

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -2,16 +2,32 @@ import jwt from "jsonwebtoken"
 
 const generateTokenAndSetCookies = async (userId, res) => {
     const accessToken = jwt.sign({userId},process.env.ACCESS_TOKEN,{
-        expiresIn: "15d",
+        expiresIn: "7d",
     })
 
     const refreshToken = jwt.sign({userId},process.env.ACCESS_TOKEN,{
-        expiresIn: "15d",
+        expiresIn: "15m",
     })
 
     return { accessToken, refreshToken }
 }
 
 export const setCookies = async (res, accessToken, refreshToken) => {
-    
+    res.cookie("accessToken", accessToken,
+        {
+            sameSite: process.env.NODE_ENV !== "development" ? "none":"lax",
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        }
+    )
+
+     res.cookie("refreshToken", refreshToken,
+        {
+            sameSite: process.env.NODE_ENV !== "development" ? "none":"lax",
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 15 * 60 * 60 * 1000
+        }
+    )
 }
