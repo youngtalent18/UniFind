27b9822ae01573a798fc36cbf6cb6f8f71c12028
Stephen Anti <stephenanti63@gmail.com>
2026-06-16 16:43:13 -0700

    1-real time message logics

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,3 +1,11 @@
 export const sendMessage = async(req, res) => {
+    try {
+        const { message } = req.body;
+        const { id } = req.params;
+        const senderId = req.user._id;
 
+        
+    } catch (error) {
+        
+    }
 }
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { sendMessage } from "../controllers/messageController";
+import { protectRoute } from "../middleware/protectRoute";
 
 const router = express.Router();
 
-router.post("/send/:id", sendMessage);
+router.post("/send/:id",protectRoute, sendMessage);
 
 export default router;
